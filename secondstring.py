157e39eaf424d816715080dbce0850670836e8fd
Tossenou <kokouvi.tossenou@sap.com>
2020-02-02 21:07:42 +0000

    added secondstring

---
+++ secondstring.py
@@ -0,0 +1,3 @@
+str = "The quick brown fox jumps over the lazy dog." # initial string
+reversed = "".join(reversed(str)) #.join() method merges all of the charactera
+print(reversed[0:43:2]) # print the reversed string
